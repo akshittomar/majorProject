a0d3031575021f5ede3dd2d0b37f452ca623939b
Akshit Tomar <akshitt125@gmail.com>
2024-02-26 09:56:44 +0530

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import Home from '../src/components/Home'
 import Navbar from '../src/components/Navbar'
+import About from './components/About'
+import CropGuide from './components/CropGuide'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   
 
   return (
-    <div>
+    <div style={{overflow:"hidden"}}  >
+      <Router>
     <Navbar></Navbar>
-    <Home></Home>
+    <Routes>
+  <Route exact path='/' element={<Home/>}/>
+  <Route exact path='/about' element={<About/>}/>
+  <Route exact path='/cropguide' element={<CropGuide/>}/>
+    </Routes>
+    </Router>
     </div>
   )
 }
