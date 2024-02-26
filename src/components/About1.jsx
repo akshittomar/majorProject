594447ee594c1026e7a7ad9d5041c239f07c40eb
sidj7 <sdzen.sid@gmail.com>
2024-02-26 09:58:10 +0530

    added about-us

---
+++ src/components/About1.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./About.css"
+function About() {
+  return (
+    <div className='section-abt-us'>
+        <div className='Heading-abt-us'>
+          heading
+        </div>
+        <div className='container-abt'>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <p>akshit tomar</p>
+          </div>
+          
+          <div className='person-card'>
+            <img src="" alt="" />
+            <p>sabhya mittal</p>
+          </div>
+
+          
+          <div className='person-card'>
+            <img src="" alt="" />
+            <p>siddharth jain</p>
+          </div>
+
+          
+          <div className='person-card'>
+            <img src="" alt="" />
+            <p>shashank prajapti</p>
+          </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
